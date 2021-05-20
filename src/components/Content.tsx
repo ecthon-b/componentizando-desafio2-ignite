472381a62992b9fa637657a61d12cc5df27bccec
Ecthon Borhis <ecthon@gmail.com>
2021-05-20 14:57:57 -0400

    Created Content and SideBar components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
-  // Complete aqui
+import { GenreResponseProps } from '../App';
+import { MovieProps } from '../App';
+import { MovieCard } from '../components/MovieCard';
+
+interface ConfigContent {
+  selectedGenre: GenreResponseProps,
+  movies: MovieProps[],
+}
+
+export function Content({selectedGenre, movies}:ConfigContent) {
+  return (
+
+  <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>    
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from 'react';
+import { GenreResponseProps } from '../App'
+import { Button } from '../components/Button'
+
+interface Config {
+  genres: GenreResponseProps[],
+  handleClickButton(id: number): void,
+}
+
+export function SideBar({genres, handleClickButton}:Config) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
